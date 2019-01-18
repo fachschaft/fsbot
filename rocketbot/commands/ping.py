f6f4861e5973fa788912acc477e2130a69eda583
smoehrle <26185187+smoehrle@users.noreply.github.com>
2019-01-18 15:01:26 +0100

    Ping pong command

--- rocketbot/commands/ping.py
+++ rocketbot/commands/ping.py
@@ -10,11 +10,19 @@ class Ping(c.Prefix):
         self.enable_public_channel = True
         self.enable_private_group = True
         self.enable_direct_message = True
-        self.prefixes.append((['ping'], self.send_pong))
+        self.prefixes.append((['ping'], self.reply_to_ping))
+        self.prefixes.append((['pong'], self.reply_to_pong))
 
     def usage(self) -> str:
         return 'ping - Reply with "pong"'
 
-    async def send_pong(self, args: str, message: m.Message) -> bool:
+    async def reply_to_ping(self, args: str, message: m.Message) -> bool:
         await self.bot.send_message(message.rid, 'Pong')
+        return True
+
+    async def reply_to_pong(self, args: str, message: m.Message) -> bool:
+        await self.bot.send_message(
+            message.rid,
+            '[Pong](https://en.wikipedia.org/wiki/Pong) is one of the earliest arcade video games. ' +\
+            'It is a table tennis sports game featuring simple two-dimensional graphics...')
         return True
