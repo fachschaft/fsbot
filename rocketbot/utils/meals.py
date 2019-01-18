c950a4e24c53fca1edb6b35e70c4431bac0890fd
Trigrab <trigrab@gmail.com>
2019-01-18 10:22:28 +0100

    add meals command

---
+++ rocketbot/utils/meals.py
@@ -0,0 +1,26 @@
+import json
+import urllib.request
+import bot_config as c
+
+
+class FoodManager:
+
+    @staticmethod
+    async def get_food(days=None):
+        if days is None:
+            days = 1
+        url = c.MENSA_CACHE_URL + '/' + str(days)
+        print(url)
+        with urllib.request.urlopen(url) as url:
+            data = json.loads(url.read().decode())
+
+        foodmsg = "```\n"
+        for day in data:
+            foodmsg += day + "\n"
+            for i, meal in enumerate(data[day]):
+                foodmsg += "  Meal: " + str(i) + "\n"
+                for line in meal['meals']:
+                    foodmsg += "    " + line + "\n"
+        foodmsg += "```\n"
+
+        return foodmsg
