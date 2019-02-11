822d45553da7613c382c3dfe9f48d9082c310297
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2019-02-11 14:47:11 +0100

    Make full dmsclient commands available

--- rocketbot/commands/dms.py
+++ rocketbot/commands/dms.py
@@ -1,45 +1,55 @@
 from typing import List, Tuple
 import subprocess
 import os
+import re
 
 import dmsclient as dms
 
 import rocketbot.commands as c
 import rocketbot.master as master
 import rocketbot.models as m
-import bot_config
 
 
-class DMSClient(c.BaseCommand):
-    def __init__(self, master: master.Master):
-        self._create_dmsclient_config_if_missing()
+class DMS(c.BaseCommand):
+    def __init__(self, master: master.Master, token: str):
+        self._create_dmsclient_config_if_missing(token)
         self.master = master
 
     def usage(self) -> List[Tuple[str, str]]:
         return [
-            ('order <product name>', 'Orders product in dms for yourself.'),
+            ('order', 'Orders product in dms for yourself.'),
         ]
 
     def can_handle(self, command: str) -> bool:
         """Check whether the command is applicable
         """
-        return command in ['order']
+        return command in ['dms', 'drink', 'drinks', 'order']
 
     async def handle(self, command: str, args: str, message: m.Message) -> None:
         """Handle the incoming message
         """
-        if command == 'order':
+        argv = re.split(r'\s+', args.strip())
+
+        userargv = [arg for arg in argv if arg.startswith('-u') or arg.startswith('--user')]
+        if len(userargv) == 0:
             userid = message.u.username
-            dms_result = subprocess.run(['dms', 'order', *args,
-                                         '--force', '--user={}'.format(userid)],
+            argv.append('--user={}'.format(userid))
+        if '--force' not in argv:
+            argv.append('--force')
+
+        if command == 'order':
+            argv = [command, *argv]
+
+        if command in ('dms', 'drink', 'drinks'):
+            dms_result = subprocess.run(['dms', *argv],
                                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             result_str = dms_result.stdout.decode('utf-8')
             await self.master.client.send_message(message.rid, result_str)
 
-    def _create_dmsclient_config_if_missing():
+    def _create_dmsclient_config_if_missing(self, token: str):
         rcfile = os.path.expanduser('~/.dmsrc')
         config = dms.DmsConfig()
         status = config.read(rcfile)
         if status == dms.ReadStatus.NOT_FOUND:
-            config._set(dms.Sec.GENERAL, 'token', bot_config.DMS_TOKEN)
+            config._set(dms.Sec.GENERAL, 'token', token)
             config.write(rcfile)
