578ad10532ff457fb406c0fee4a7b5fed1066919
smoehrle <26185187+smoehrle@users.noreply.github.com>
2019-02-27 14:52:06 +0100

    Few more tests

--- tests/unit/test_bots.py
+++ tests/unit/test_bots.py
@@ -1,14 +1,16 @@
+import pytest
+
 import rocketbot.bots.accesscontrol as ac
 import rocketbot.bots.base as base
 import rocketbot.models as m
 
 
-class Basebot(base.BaseBot):
+class BaseBot(base.BaseBot):
     async def handle(self, message: m.Message) -> None:
         return
 
 
-class WhiteListBot(ac.WhitelistRoomMixin, Basebot):
+class WhiteListBot(ac.WhitelistRoomMixin, BaseBot):
     pass
 
 
@@ -19,3 +21,37 @@ def test_whitelistbot() -> None:
 
     assert bot.is_applicable(r1) is True
     assert bot.is_applicable(r2) is False
+
+
+class BlacklistBot(ac.BlacklistRoomMixin, BaseBot):
+    pass
+
+
+def test_blacklistbot() -> None:
+    bot = BlacklistBot(master=None, blacklist=['room1'])
+    r1 = m.RoomRef2(roomType=m.RoomType.PUBLIC, roomParticipant=True, roomName='room1')
+    r2 = m.RoomRef2(roomType=m.RoomType.PUBLIC, roomParticipant=True, roomName='room2')
+
+    assert bot.is_applicable(r1) is False
+    assert bot.is_applicable(r2) is True
+
+
+class RoomTypeBot(ac.RoomTypeMixin, BaseBot):
+    pass
+
+
+def test_roomtypebot_argsrequired() -> None:
+    with pytest.raises(ValueError):
+        RoomTypeBot(master=None)
+
+
+def test_roomtypebot() -> None:
+    bot = RoomTypeBot(master=None, enable_public_channel=True)
+
+    r1 = m.RoomRef2(roomType=m.RoomType.PUBLIC, roomParticipant=True, roomName='room1')
+    r2 = m.RoomRef2(roomType=m.RoomType.PRIVATE, roomParticipant=True, roomName='room2')
+    r3 = m.RoomRef2(roomType=m.RoomType.DIRECT, roomParticipant=True, roomName='room3')
+
+    assert bot.is_applicable(r1) is True
+    assert bot.is_applicable(r2) is False
+    assert bot.is_applicable(r3) is False
