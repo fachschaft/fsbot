d2c58fd4b256223ba3ed951fe0ad92e0fdef2fd8
smoehrle <26185187+smoehrle@users.noreply.github.com>
2019-03-02 16:45:00 +0100

    Add basic client tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,41 @@
+import asyncio
 import datetime
+from typing import Iterable
 
 import pytest
+from rocketchat_API.rocketchat import RocketChat
 
+import rocketbot.master as master
 import rocketbot.models as m
+
+
+@pytest.yield_fixture
+def event_loop() -> Iterable[asyncio.AbstractEventLoop]:
+    """Create an instance of the default event loop for each test case.
+    Fixes: https://github.com/pytest-dev/pytest-asyncio/issues/29
+    """
+    policy = asyncio.get_event_loop_policy()
+    res = policy.new_event_loop()
+    asyncio.set_event_loop(res)
+    _close = res.close
+    # Ignore mypy bug/limitation -> https://github.com/python/mypy/issues/2427
+    res.close = lambda: None  # type: ignore
+
+    yield res
+
+    _close()
+
+
+@pytest.fixture
+def masterbot(event_loop: asyncio.AbstractEventLoop) -> master.Master:
+    rocket = RocketChat()
+    username = 'testbot'
+    name = 'saBOTeur'
+    password = '1234'
+    email = 'email@domain.com'
+    rocket.users_register(email=email, name=name, password=password, username=username)
+
+    return master.Master('http://localhost:3000', username, password, tls=False, loop=event_loop)
 
 
 @pytest.fixture
---
+++ tests/integration/test_ddpclient.py
@@ -0,0 +1,46 @@
+import asyncio
+from typing import AsyncIterable, Dict
+
+import pytest
+from rocketchat_API.rocketchat import RocketChat
+
+import rocketbot.client as client
+
+
+@pytest.yield_fixture
+async def ddpclient(event_loop: asyncio.AbstractEventLoop) -> AsyncIterable[client.Client]:
+    ddpclient = client.Client('ws://localhost:3000/websocket', event_loop)
+    await ddpclient.connect()
+
+    yield ddpclient
+
+    await ddpclient.disconnect()
+
+
+@pytest.fixture
+def exisiting_user() -> Dict[str, str]:
+    rocket = RocketChat()
+    user = {
+        'username': 'testbot',
+        'name': 'saBOTeur',
+        'password': '1234',
+        'email': 'email@domain.com',
+    }
+    rocket.users_register(**user)
+    return user
+
+
+@pytest.mark.asyncio
+async def test_basic_connect_disconnect(event_loop: asyncio.AbstractEventLoop) -> None:
+    ws_url = 'ws://localhost:3000/websocket'
+    ddpclient = client.Client(ws_url, event_loop)
+
+    await ddpclient.connect()
+
+    await ddpclient.disconnect()
+
+
+@pytest.mark.asyncio
+async def test_login(ddpclient: client.Client, exisiting_user: Dict[str, str]) -> None:
+    await ddpclient.login(exisiting_user['username'], exisiting_user['password'])
+    # TODO check result (esp. if all fields are mapped)
